67fa76b06e4b4306f0c7ebbd4b383d5e88adcde1
fujitani sora <fujitanisora0414@gmail.com>
2026-03-26 19:04:17 +0900

    memoli 1.2.0: switch to tar.gz assets

--- Formula/memoli.rb
+++ Formula/memoli.rb
@@ -5,22 +5,23 @@ class Memoli < Formula
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/sorafujitani/memoli/releases/download/v#{version}/memoli-darwin-arm64"
-    sha256 "09d27939ed72c15076aff4a19c92e4292883bd091736c8cccaf867f4b215f660"
+    url "https://github.com/sorafujitani/memoli/releases/download/v#{version}/memoli-darwin-arm64.tar.gz"
+    sha256 "e36e6e66ac47e7b33561a41f09cbc3daeac2d2a205efaa7533b627494b97b31d"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/sorafujitani/memoli/releases/download/v#{version}/memoli-darwin-x64"
-    sha256 "f0aa8827685b4494abb93f5798ffbab096be4a6b6df163663e02c7e6e3781395"
+    url "https://github.com/sorafujitani/memoli/releases/download/v#{version}/memoli-darwin-x64.tar.gz"
+    sha256 "79298b3f87225ca674755c9fd1d4498dd494706806771694a928e445f67c8ffc"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/sorafujitani/memoli/releases/download/v#{version}/memoli-linux-arm64"
-    sha256 "fe43f9b2f05086a2990de70ef13e452f920622420d98319e13e0a99baaf35e4b"
+    url "https://github.com/sorafujitani/memoli/releases/download/v#{version}/memoli-linux-arm64.tar.gz"
+    sha256 "39ec4ae5cf5b2aee8d922993c92c9c63395a90b90f114b36d2b579687b0cee94"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/sorafujitani/memoli/releases/download/v#{version}/memoli-linux-x64"
-    sha256 "c60e3987eb3a87f68ba8c6251e9fb4fdef67a8da1b49c560c6c4f97fa5166c90"
+    url "https://github.com/sorafujitani/memoli/releases/download/v#{version}/memoli-linux-x64.tar.gz"
+    sha256 "05c19e1cdfe3011b69caa661c1bec5909d80dadcdf07a715e0ef807981c937be"
   end
 
   def install
-    binary_name = stable.url.split("/").last
-    bin.install binary_name => "memoli"
+    # tar.gz extracts to memoli-<platform>, rename to memoli
+    binary = Dir["memoli-*"].first
+    bin.install binary => "memoli"
   end
 
   test do
